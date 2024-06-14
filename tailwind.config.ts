ea3b0d6507ef406f355dddd7956defd954fff2da
lengsukq <lengsukq@foxmail.com>
2024-06-14 13:18:28 +0800

    nextui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import {nextui} from '@nextui-org/theme';
 import type { Config } from "tailwindcss"
 
 const config = {
@@ -7,7 +8,8 @@ const config = {
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-	],
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+  ],
   prefix: "",
   theme: {
     container: {
@@ -74,7 +76,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [nextui(),require("tailwindcss-animate")],
 } satisfies Config
 
 export default config
